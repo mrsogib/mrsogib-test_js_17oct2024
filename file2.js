43f4d25b43972f4c8b1bb6d51d0b700a6620d684
mrSogib <83939055+mrsogib@users.noreply.github.com>
2024-10-17 14:32:03 +0000

    comparison of wired data types

---
+++ file2.js
@@ -0,0 +1,20 @@
+// console.log(1>2);
+// console.log(1>=2);
+// console.log(1<2);
+
+// console.log(1<"2");
+// console.log("1">=2);
+// console.log('1'<=2);
+
+// console.log(null>0);
+// console.log(null<0);
+// console.log(null==0);
+// console.log(null!=0); // null != 0
+// console.log(null>=0);
+// console.log(null<=0);
+
+console.log(undefined>0);
+console.log(undefined<0);
+console.log(undefined==0);
+console.log(undefined>=0);
+console.log(undefined<=0);
