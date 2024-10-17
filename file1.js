71055d1edd1aeae2bf20eabf34721047994b66d1
mrSogib <83939055+mrsogib@users.noreply.github.com>
2024-10-17 13:58:19 +0000

    types of data in javascript

---
+++ file1.js
@@ -0,0 +1,13 @@
+"use strict" // treats all js code as new version syntaxt
+
+// console.table(['account', 'myname', "hello"]); //very useful logging style
+
+// alert(3+3); // old school, better not to use now! browser will still run, node won't
+
+//// datatypes
+// let state; // undefined is a standAlone datatype value = empty
+// let state2=null; // null is a standAlone value, type = object
+// console.log(typeof state, state2);
+// symbol datatype is for uniqueness works
+// Number("33") = 33, Number("33dsad") = NaN
+// "NaN" type is a number type data even though that means Not A Number; 
